98b89f34bda4866e0f2f5d0de98136d13252cc0f
David <davidmmy@gmail.com>
2024-03-27 16:31:02 +0200

    Fix notes time

--- src/components/profile/User.tsx
+++ src/components/profile/User.tsx
@@ -1,95 +1,103 @@
-import useUser from '@/hooks/useUser';
-import { type FC, useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
+import useUser from "@/hooks/useUser";
+import { type FC, useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
-interface userProps { }
+interface userProps {}
 
 export interface Note {
-    doctor: { name: string, id: string }
-    date: Date
-    body: string
+  doctor: { name: string; id: string };
+  date: Date;
+  body: string;
 }
 
+const UserProfile: FC<userProps> = ({}) => {
+  const { user, loading } = useUser();
+  const router = useRouter();
+  const [notes, setNotes] = useState<Note[]>([]);
+  const [loadingNotes, setLoading] = useState(false);
 
-const UserProfile: FC<userProps> = ({ }) => {
-    const { user, loading } = useUser()
-    const router = useRouter()
-    const [notes, setNotes] = useState<Note[]>([])
-    const [loadingNotes, setLoading] = useState(false)
+  useEffect(() => {
+    if (loading || !user) return;
+    setLoading(true);
+    fetch("/api/notes/" + user?.id)
+      .then(async (response) => {
+        if (response.status === 200) {
+          let data = await response.json();
+          setNotes(data);
+        } else {
+          router.push("/login");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
-    useEffect(() => {
-        if (loading || !user) return
-        setLoading(true)
-        fetch('/api/notes/' + user?.id).then(async response => {
-            if (response.status === 200) {
-                let data = await response.json()
-                setNotes(data)
-            } else {
-                router.push('/login')
-            }
-        }).catch(error => {
-            console.log(error)
-        })
+    setLoading(false);
+  }, [user]);
 
-        setLoading(false)
-    }, [user])
+  if (loading || loadingNotes) return "Loading...";
 
-    if (loading) return
+  if (user) {
+    return (
+      <div className="flex justify-center items-center h-full">
+        <div className="space-y-4 w-11/12 max-w-[400px] p-4 bg-white shadow-sm rounded-md">
+          <div className="text-center font-bold text-lg text-primary">
+            {user.name}
+          </div>
+          <div className="">Doctors Notes</div>
+          <div>
+            <ul className="space-y-4">
+              {notes.map((note) => {
+                if (!note) return;
+                let date = new Date(note.date); //.toLocaleDateString('en',{day:'numeric', month:'numeric'})
+                let today = new Date();
 
-    if (user) {
-        return (
-            <div className='flex justify-center items-center h-full'>
-                <div className='space-y-4 w-11/12 max-w-[400px] p-4 bg-white shadow-sm rounded-md'>
-                    <div className='text-center font-bold text-lg text-primary'>{user.name}</div>
-                    <div className=''>Doctors Notes</div>
-                    <div>
-                        <ul className='space-y-4'>
-                            {notes.map((note) => {
-                                if (!note) return
-                                let date = new Date(note.date)//.toLocaleDateString('en',{day:'numeric', month:'numeric'})
-                                let today = new Date()
+                let def: number = (today as any) - (date as any);
+                def = Math.floor(def / 1000);
+                let display = def + " secs ago";
 
-                                let def: number = (today as any) - (date as any)
-                                def = Math.floor(def / 1000)
-                                let display = def + ' secs ago'
+                if (def >= 60) {
+                  def = Math.floor(def / 60);
+                  display = def + " mins ago";
+                }
+                if (def >= 60 * 60) {
+                  def = Math.floor(def / (60 * 60));
+                  display = def + " hrs ago";
+                }
+                if (def >= 24 * 60 * 60) {
+                  def = Math.floor(def / (24 * 60 * 60));
+                  display = def + " days ago";
+                }
+                if (def >= 31 * 24 * 60 * 60) {
+                  def = Math.floor((def / 31) * 24 * 60 * 60);
+                  display = def + " months ago";
+                }
 
-                                if (def >= 60) {
-                                    def = Math.floor(def / 60)
-                                    display = def + ' mins ago'
-                                }
-                                if (def >= 60) {
-                                    def = Math.floor(def / 60)
-                                    display = def + ' hrs ago'
-                                }
-                                if (def >= 24) {
-                                    def = Math.floor(def / 24)
-                                    display = def + ' days ago'
-                                }
-                                if (def >= 31) {
-                                    def = Math.floor(def / 30)
-                                    display = def + ' months ago'
-                                }
-
-                                return (
-                                    <li className='p-2 rounded-md bg-highlight'>
-                                        <div className='space-y-2'>
-                                            <div className='flex justify-between items-center'>
-                                                <div className='text-base text-primary'>{note.doctor.name}</div>
-                                                <div className='text-xs'>{display}</div>
-                                            </div>
-                                            <p className='text-sm'>{note.body}</p>
-                                        </div>
-                                    </li>
-                                )
-                            })}
-                            <li className='p-2 rounded-md'>
-                                <p className='text-gray-400 text-xs'>This is a non-doctor's Profile Page and listed all notes for this user.</p>
-                            </li>
-                        </ul>
+                return (
+                  <li className="p-2 rounded-md bg-highlight">
+                    <div className="space-y-2">
+                      <div className="flex justify-between items-center">
+                        <div className="text-base text-primary">
+                          {note.doctor.name}
+                        </div>
+                        <div className="text-xs">{display}</div>
+                      </div>
+                      <p className="text-sm">{note.body}</p>
                     </div>
-                </div>
-            </div>
-        );
-    }
-}
+                  </li>
+                );
+              })}
+              <li className="p-2 rounded-md">
+                <p className="text-gray-400 text-xs">
+                  This is a non-doctor's Profile Page and listed all notes for
+                  this user.
+                </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
 export default UserProfile;
--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -1,14 +1,19 @@
 export const navList = [
-    {
-        name: 'Home',
-        to: '#home'
-    },
-    {
-        name: 'About',
-        to: '#about'
-    },
-    {
-        name: 'Doctors',
-        to: '/doctors'
-    }
-]
+  {
+    name: "Home",
+    to: "#home",
+  },
+  {
+    name: "About",
+    to: "#about",
+  },
+  {
+    name: "Doctors",
+    to: "/doctors",
+  },
+];
+
+export type navItem = {
+  name: string;
+  to: string;
+};
